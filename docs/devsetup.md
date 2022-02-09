f3a7c2704bbaeef551fbd0d073d0de6b3c09fcb7
Armin Reiter <armin.reiter@live.at>
2022-02-09 18:54:23 +0100

    initial checkin angular app

---
+++ docs/devsetup.md
@@ -0,0 +1,6 @@
+# Developer setup
+
+Install:
+
+- nodejs (17.4.0+)
+- Angular (npm install -g @angular/cli)
---
+++ src/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'w3tk-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'web3toolkit';
+}
