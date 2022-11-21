6519e8a4a84e5db86a0c9d40d566c9cc221d00d7
Armin Reiter <armin.reiter@live.at>
2022-11-21 11:01:34 +0100

    add tool get address from private key

--- src/src/app/tools/tools-routing.module.ts
+++ src/src/app/tools/tools-routing.module.ts
@@ -9,6 +9,7 @@ import { ToolsOverviewComponent } from './tools-overview/tools-overview.componen
 import { GetBalanceComponent } from './get-balance/get-balance.component';
 import { GetAddressKeysFromSeedComponent } from './get-address-keys-from-seed/get-address-keys-from-seed.component';
 import { DrainfundsComponent } from './drainfunds/drainfunds.component';
+import { GetAddressFromKeyComponent } from './get-address-from-key/get-address-from-key.component';
 
 const routes: Routes = [
     {
@@ -17,6 +18,7 @@ const routes: Routes = [
             { path: 'genseedphrase', component: GenSeedPhraseComponent },
             { path: 'getaddrfromseed', component: GetAddressFromSeedComponent },
             { path: 'getaddrkeysfromseed', component: GetAddressKeysFromSeedComponent },
+            { path: 'getaddrfromkey', component: GetAddressFromKeyComponent},
             { path: 'weiconverter', component: WeiConverterComponent },
             { path: 'getbalances', component: GetBalanceComponent },
             { path: 'drainfunds', component: DrainfundsComponent}
